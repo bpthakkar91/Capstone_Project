1adc549eb19ce14021460ef97ff974fee4b97ea4
Bhavin Thakkar <bpthakkar91@gmail.com>
2022-03-06 09:42:04 -0600

    React version
    Added React Version of Index.html page

---
+++ my-app/src/App.js
@@ -0,0 +1,62 @@
+//import './App.css';
+
+import './project.css';
+
+function App() {
+  return (
+    <div className="App">
+              <div id="wrapper">
+          <header>
+          <h1> FOOD HUB</h1>
+          </header>
+          <nav>
+            
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="menu.html">Menu</a></li>
+              <li><a href="events.html">Events</a></li>
+              <li><a href="membership.html">Membership</a></li>
+              <li><a href="calendar.html">Calendar</a></li>
+              <li><a href="template.html">About Us</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+
+          </nav>
+          <main>
+          <div id="heroroad">
+          </div>
+
+          <h2> Daily Food </h2>
+          <p>
+
+          <h3>Food Hub</h3>
+          </p>
+
+          </main>
+
+          <footer>
+              Copyright &copy; 2022 Capstone<br />
+              <script type="text/javascript">
+                          showEM("alegna","moc.lpm");
+                </script>
+          <h3>This page was last modified on:
+          <script>
+          document.write(document.lastModified);
+
+          </script>
+          </h3>
+
+          </footer>
+
+
+
+          </div>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
